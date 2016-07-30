7996f481efd943bd97e0576cedece3cd61fa7f69
Siddhant S Goral <jaiyeshsidh@gmail.com>
2016-07-30 11:43:15 +0530

    Read More Dialog

--- javascript.js
+++ javascript.js
@@ -99,3 +99,21 @@ jQuery(function ($) {
 	$this.countTo(options);
   }
 });
+
+// -------------------------------------------------------------------------
+// Navbar Drop
+// -------------------------------------------------------------------------
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 750);
+          return false;
+        }
+      }
+    });
+  });
